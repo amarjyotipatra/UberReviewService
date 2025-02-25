68fa9b0ec25e8b645b02497364e0cf2371011ba1
Amar Jyoti Patra <amarjyotipatra511@gmail.com>
2025-02-25 17:54:11 +0530

    Added more columns for review table

--- src/main/java/com/example/uberreviewservice/models/Review.java
+++ src/main/java/com/example/uberreviewservice/models/Review.java
@@ -1,11 +1,31 @@
 package com.example.uberreviewservice.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
 
 @Entity
+@Table(name = "bookingreview")
 public class Review {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //Identity is auto-incremented
     Long id;
+
+    @Column(nullable = false)
+    String content;
+
+    Double rating;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(nullable = false)
+    @LastModifiedDate
+    Date updatedAt;
 }
